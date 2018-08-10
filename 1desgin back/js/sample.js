03f898a6d30ec4f2d8386565196883800ce8f6cc
rcrm1234567 <rcrmxtensible@gmail.com>
2018-08-10 17:36:31 +0530

    1desing on git

---
+++ 1desgin back/js/sample.js
@@ -0,0 +1,26 @@
+
+jQuery(document).ready(function($) {
+  $('#left').simpleScrollFollow({    
+    min_width: 992,
+    limit_elem: $('article')
+  });
+  
+  $('#right').simpleScrollFollow({
+    min_width: 992,
+    limit_elem: $('article')
+  });
+
+  $('#isolated').simpleScrollFollow({
+    min_width: 992,
+    limit_elem: $('article')
+  });
+  $('#isolated #toggle_scroll').click(function() {
+    if ($(this).text() == 'click to disable scroll') {
+      $('#isolated').simpleScrollFollow('setEnabled', false);
+      $(this).text('click to enable scroll');
+    } else {
+      $('#isolated').simpleScrollFollow('setEnabled', true);
+      $(this).text('click to disable scroll');
+    }
+  });
+});
